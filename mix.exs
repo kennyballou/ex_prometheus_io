1480de1b091ea7f83f2d422d9db910ab5b64c176
kballou <kballou@devnulllabs.io>
2016-03-25 16:28:36 -0600

    Update version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExPrometheusIo.Mixfile do
     [app: :ex_prometheus_io,
      description: description,
      package: package,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
